abe67e75719fcdbf9b990795ac7f8f9a872cb77c
yaros <yaroslav.panasiuk.1902@gmail.com>
2023-10-09 17:51:10 +0300

    0.0.2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 import os
-
 import os.path
+import traceback
+
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, ConversationHandler
 from google.auth.transport.requests import Request
@@ -10,7 +11,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-ENTER_NAME, ENTER_PHONE, ENTER_SEX, ENTER_UNI, ENTER_COURSE, ENTER_VISITED, ENTER_HOW_COME, ENTER_ENGLISH_LEVEL, ENTER_RELIGIOUS, EXIT_CONVERSATION = range(10)
+ENTER_NAME, ENTER_PHONE, ENTER_SEX, ENTER_UNI, ENTER_COURSE, ENTER_VISITED, SPECIFY_VISITED, ENTER_HOW_COME, ENTER_ENGLISH_LEVEL, ENTER_RELIGIOUS, EXIT_CONVERSATION = range(
+    11)
 
 
 class Student:
@@ -25,9 +27,10 @@ class Student:
         self.visited = '-'
         self.how_come = '-'
         self.english_level = '-'
+        self.religious = '-'
 
     def to_str(self):
-        return self.id + ', ' + self.name + ', ' + self.phone + ', ' + self.nickname + ', ' + self.sex + ', ' + self.uni + ', ' + self.course + ', ' + self.visited + ', ' + self.how_come + ', ' + self.english_level
+        return self.id + ', ' + self.name + ', ' + self.phone + ', ' + self.nickname + ', ' + self.sex + ', ' + self.uni + ', ' + self.course + ', ' + self.visited + ', ' + self.how_come + ', ' + self.english_level + ', ' + self.religious
 
 
 user = Student()
@@ -56,7 +59,7 @@ def read_config(value) -> str:
     return ''
 
 
-def add_to_spreadsheets(data: str):
+def add_to_spreadsheets():
     creds = None
     scopes = [read_config("SCOPES")]
     if os.path.exists('token.json'):
@@ -74,10 +77,9 @@ def add_to_spreadsheets(data: str):
     try:
         service = build('sheets', 'v4', credentials=creds)
         sheet = service.spreadsheets()
-        values = data.split(', ')
         students = sheet.values().get(spreadsheetId=read_config("SAMPLE_SPREADSHEET_ID"),
                                       range=read_config("SAMPLE_RANGE_NAME")).execute()
-        data_range = 'A{0}:J{0}'.format(str(len(students.get('values', [])) + 1))
+        data_range = 'A{0}:K{0}'.format(str(len(students.get('values', [])) + 1))
         range_body_values = {
             'value_input_option': 'USER_ENTERED',
             'data': [
@@ -85,11 +87,88 @@ def add_to_spreadsheets(data: str):
                     'majorDimension': 'ROWS',
                     'range': data_range,
                     'values': [
-                        [values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9]]
+                        [user.id, user.name, user.nickname, user.phone, user.sex, user.uni, user.course,
+                         user.visited, user.how_come, user.english_level, user.religious]
                     ]
                 },
             ]}
-        service.spreadsheets().values().batchUpdate(spreadsheetId=read_config("SAMPLE_SPREADSHEET_ID"), body=range_body_values).execute()
+        service.spreadsheets().values().batchUpdate(spreadsheetId=read_config("SAMPLE_SPREADSHEET_ID"),
+                                                    body=range_body_values).execute()
+    except HttpError as err:
+        print(err)
+
+
+def get_questions():
+    creds = None
+    scopes = [read_config("SCOPES")]
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', scopes)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', scopes)
+            creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+        sheet = service.spreadsheets()
+        questions = sheet.values().get(spreadsheetId=read_config("SAMPLE_SPREADSHEET_ID"),
+                                       range='Питання!B1:B20').execute()
+        values = questions.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+        result = []
+        for value in values:
+            result.append(value[0])
+        return result
+
+    except HttpError as err:
+        print(err)
+
+
+questions = get_questions()
+
+
+def get_question(num: int):
+    return questions[num]
+
+
+def get_chats():
+    creds = None
+    scopes = [read_config("SCOPES")]
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', scopes)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', scopes)
+            creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+        sheet = service.spreadsheets()
+        questions = sheet.values().get(spreadsheetId=read_config("SAMPLE_SPREADSHEET_ID"),
+                                       range='Реєстрація!A1:A1000').execute()
+        values = questions.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+        result = []
+        for value in values:
+            result.append(value[0])
+        return result
+
     except HttpError as err:
         print(err)
 
@@ -108,14 +187,6 @@ def build_menu(button_names):
     return buttons
 
 
-def stop_messaging_in_church_chat(update, context):
-    if update.message.chat_id != int(read_config('ADMIN_ID')):
-        return
-    jobs_to_delete = context.job_queue.get_jobs_by_name(read_config('CHURCH_CHAT_ID'))
-    for i in range(len(jobs_to_delete)):
-        jobs_to_delete[i].schedule_removal()
-
-
 def get_chat_id(update, context):
     if update.message.chat_id != int(read_config('ADMIN_ID')):
         return
@@ -124,19 +195,13 @@ def get_chat_id(update, context):
 
 def start_command(update, context):
     keyboard = [[KeyboardButton('Зареєструватись🙌')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text=read_config('INTRODUCTION_TEXT'),
+    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(0),
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
 
 
-def message_handler(update, context):
-    match update.message.text:
-        case 'get_id':
-            context.bot.send_message(chat_id=update.message.chat_id, text=str(update.message.chat_id))
-
-
 def register(update, context):
-    keyboard = [[KeyboardButton('Yes!')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text=read_config('REGISTER_TEXT'),
+    keyboard = [[KeyboardButton('Так!')]]
+    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(1),
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     user.id = str(update.message.chat_id)
     return ENTER_NAME
@@ -144,26 +209,28 @@ def register(update, context):
 
 def ask_name(update, context):
     user.nickname = update.message.from_user.username
-    update.message.reply_text('Окей, тоді почнемо. Напиши своє прізвище та ім’я у форматі “Ім`я Прізвище”')
+    update.message.reply_text(get_question(2))
     return ENTER_PHONE
 
 
 def ask_phone(update, context):
-    name = update.message.text.split()
-    if len(name) < 2 or (not (name[0].isalpha() and name[1].isalpha())):
-        return ask_name(update, context)
-    user.name = update.message.text
-    update.message.reply_text('Номер телефону у форматі "380938351301"')
+    if user.name == '-':
+        name = update.message.text.split()
+        if len(name) < 2 or (not (name[0].isalpha() and name[1].isalpha())):
+            return ask_name(update, context)
+        user.name = update.message.text
+    update.message.reply_text(get_question(3))
     return ENTER_SEX
 
 
 def ask_sex(update, context):
-    phone = update.message.text
-    if not (len(phone) == 12 and phone.isdigit()):
-        return ask_phone(update, context)
-    user.phone = phone
-    keyboard = [[KeyboardButton('Чол')], [KeyboardButton('Жін')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text="Стать",
+    if user.phone == '-':
+        phone = update.message.text
+        if not (len(phone) == 12 and phone.isdigit()):
+            return ask_phone(update, context)
+        user.phone = phone
+    keyboard = [[KeyboardButton('Чол'), KeyboardButton('Жін')]]
+    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(4),
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return ENTER_UNI
 
@@ -181,37 +248,45 @@ def ask_uni(update, context):
                 [KeyboardButton('Українська Академія Друкарства')],
                 ]
     context.bot.send_message(chat_id=update.message.chat_id,
-                             text="Оберіть свій універ. Якщо вашого навчального закладу немає у списку - впишіть його вручну",
+                             text=get_question(5),
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return ENTER_COURSE
 
 
 def ask_course(update, context):
     user.uni = update.message.text
-    keyboard = [[KeyboardButton('1')], [KeyboardButton('2')], [KeyboardButton('3')],
-                [KeyboardButton('4')], [KeyboardButton('5')], [KeyboardButton('6')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text="курс",
+    keyboard = [[KeyboardButton('1'), KeyboardButton('2'), KeyboardButton('3')],
+                [KeyboardButton('4'), KeyboardButton('5'), KeyboardButton('6')],
+                [KeyboardButton('Закінчив'), KeyboardButton('Школяр')]]
+    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(6),
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return ENTER_VISITED
 
 
 def ask_visited(update, context):
     course = update.message.text
-    if not course.isdigit():
-        return ask_course(update, context)
     user.course = course
     user.course = update.message.text
-    keyboard = [[KeyboardButton('Так')], [KeyboardButton('НІ')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text="чи відвідував поекти?",
+    keyboard = [[KeyboardButton('Так'), KeyboardButton('НІ')]]
+    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(7),
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
+    return SPECIFY_VISITED
+
+
+def specify_visited(update, context):
+    user.visited = update.message.text
+    if not (user.visited == 'Так'):
+        return ask_how_come(update, context)
+    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(8))
     return ENTER_HOW_COME
 
 
 def ask_how_come(update, context):
-    user.visited = update.message.text
+    if user.visited == 'Так':
+        user.visited = update.message.text
     keyboard = [[KeyboardButton('Флаєр')], [KeyboardButton('Постер')], [KeyboardButton('Друзі запросили')],
                 [KeyboardButton('Реклама Instagram')], [KeyboardButton('Реклама Telegram')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text="як дізнався про ак?",
+    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(9),
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return ENTER_ENGLISH_LEVEL
 
@@ -224,36 +299,45 @@ def ask_english_level(update, context):
                 [KeyboardButton('можу вести діалог, але слово consciousness буду гуглити')],
                 [KeyboardButton('дивлюсь серіали англійською без субтитрів')],
                 [KeyboardButton('англійська майже як рідна')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text="Який рівень англ?",
+    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(10),
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return ENTER_RELIGIOUS
 
 
 def ask_religious(update, context):
-    user.how_come = update.message.text
+    user.english_level = update.message.text
     keyboard = [[KeyboardButton('Позитивно')],
                 [KeyboardButton('Негативно')],
-                [KeyboardButton('Нейтрально')],]
-    context.bot.send_message(chat_id=update.message.chat_id, text="Який рівень англ?",
+                [KeyboardButton('Нейтрально')], ]
+    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(11),
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return EXIT_CONVERSATION
 
 
 def exit_conversation(update, context):
-    user.english_level = update.message.text
-    update.message.reply_text(user.to_str())
-    add_to_spreadsheets(user.to_str())
+    user.religious = update.message.text
+    update.message.reply_text(get_question(12))
+    add_to_spreadsheets()
     return ConversationHandler.END
+
+
+def spam_message(update, context):
+    if update.message.chat_id != int(read_config('ADMIN_ID')):
+        return
+    try:
+        context.bot.send_message(chat_id=int(read_config('ADMIN_ID')), text="sent")
+    except:
+        context.bot.send_message(chat_id=int(read_config('ADMIN_ID')), text=traceback.format_exc())
 
 
 def main():
     print("start")
-    updater = Updater(read_config("BOT_TOKEN"), use_context=True)
+    updater = Updater(read_config("TEST_BOT_TOKEN"), use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start_command))
     dispatcher.add_handler(CommandHandler('print_jobs', print_jobs))
     dispatcher.add_handler(CommandHandler('link_to_spreadsheets', link_to_spreadsheets))
-    # dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
+    dispatcher.add_handler(MessageHandler(Filters.text('start_messaging_in_church_chat'), spam_message))
     conversation_handler = ConversationHandler(
         entry_points=[MessageHandler(Filters.text('Зареєструватись🙌'), register)],
         states={
@@ -263,6 +347,7 @@ def main():
             ENTER_UNI: [MessageHandler(Filters.all, ask_uni)],
             ENTER_COURSE: [MessageHandler(Filters.all, ask_course)],
             ENTER_VISITED: [MessageHandler(Filters.all, ask_visited)],
+            SPECIFY_VISITED: [MessageHandler(Filters.all, specify_visited)],
             ENTER_HOW_COME: [MessageHandler(Filters.all, ask_how_come)],
             ENTER_ENGLISH_LEVEL: [MessageHandler(Filters.all, ask_english_level)],
             ENTER_RELIGIOUS: [MessageHandler(Filters.all, ask_religious)],
