10ca03fbef82dc2ce9810b554cf540de257f7712
yaros <yaroslav.panasiuk.1902@gmail.com>
2023-10-14 14:03:51 +0300

    '0.0.6'

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from __future__ import print_function
+
+import asyncio
 import os
 import os.path
+import time
 import traceback
+from uuid import uuid4
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, ConversationHandler
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -49,7 +53,7 @@ def read_config(value) -> str:
     return ''
 
 
-def add_to_spreadsheets():
+def add_to_spreadsheets(data: []):
     creds = None
     scopes = [read_config("SCOPES")]
     if os.path.exists('token.json'):
@@ -77,8 +81,7 @@ def add_to_spreadsheets():
                     'majorDimension': 'ROWS',
                     'range': data_range,
                     'values': [
-                        [user.id, user.name, user.nickname, user.phone, user.sex, user.uni, user.course,
-                         user.visited, user.how_come, user.english_level, user.religious]
+                        data
                     ]
                 },
             ]}
@@ -107,7 +110,7 @@ def get_questions():
         service = build('sheets', 'v4', credentials=creds)
         sheet = service.spreadsheets()
         questions = sheet.values().get(spreadsheetId=read_config("SAMPLE_SPREADSHEET_ID"),
-                                       range='Питання!B1:B30').execute()
+                                       range='Питання!B2:B30').execute()
         values = questions.get('values', [])
 
         if not values:
@@ -122,11 +125,7 @@ def get_questions():
         print(err)
 
 
-questions = get_questions()
-
-
-def get_question(num: int):
-    return questions[num]
+INTRODUCTION_TEXT, REGISTRATION_TEXT, ASK_NAME_TEXT, ASK_PHONE_TEXT, ASK_SEX_TEXT, ASK_UNI_TEXT, ASK_COURSE_TEXT, ASK_VISITED_TEXT, SPECIFY_VISITED_TEXT, ASK_HOW_COME_TEXT, ASK_ENGLISH_LEVEL_TEXT, ASK_RELIGIOUS_TEXT, END_REGISTRATION_TEXT, LOCATION_TEXT, SCHEDULE_TEXT, INTERVIEW_TEXT, TUTOR_TIME_TEXT, ABOUT_US_TEXT, CONNECT_TEXT, INVALID_REGISTRATION_TEXT = get_questions()
 
 
 def get_chats():
@@ -165,24 +164,24 @@ def get_chats():
 
 def start_command(update, context):
     keyboard = [[KeyboardButton('Зареєструватись🙌')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(1),
+    context.bot.send_message(chat_id=update.message.chat_id, text=INTRODUCTION_TEXT,
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
 
 
 def register(update, context):
     if get_chats().__contains__(str(update.message.chat_id)):
-        context.bot.send_message(chat_id=update.message.chat_id, text=get_question(20))
+        context.bot.send_message(chat_id=update.message.chat_id, text=INVALID_REGISTRATION_TEXT)
         return ConversationHandler.END
     keyboard = [[KeyboardButton('Так!')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(2),
+    context.bot.send_message(chat_id=update.message.chat_id, text=REGISTRATION_TEXT,
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
-    user.id = str(update.message.chat_id)
+    context.user_data['id'] = str(update.message.chat_id)
     return ENTER_NAME
 
 
 def ask_name(update, context):
-    user.nickname = update.message.from_user.username
-    update.message.reply_text(get_question(3))
+    context.user_data['nickname'] = update.message.from_user.username
+    update.message.reply_text(ASK_NAME_TEXT)
     return ENTER_PHONE
 
 
@@ -191,8 +190,8 @@ def ask_phone(update, context):
         name = update.message.text.split()
         if len(name) < 2 or (not (name[0].isalpha() and name[1].isalpha())):
             return ask_name(update, context)
-        user.name = update.message.text
-    update.message.reply_text(get_question(4))
+        context.user_data['name'] = update.message.text
+    update.message.reply_text(ASK_PHONE_TEXT)
     return ENTER_SEX
 
 
@@ -201,9 +200,9 @@ def ask_sex(update, context):
         phone = update.message.text
         if not (len(phone) == 12 and phone.isdigit()):
             return ask_phone(update, context)
-        user.phone = phone
+        context.user_data['phone'] = phone
     keyboard = [[KeyboardButton('Чол'), KeyboardButton('Жін')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(5),
+    context.bot.send_message(chat_id=update.message.chat_id, text=ASK_SEX_TEXT,
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return ENTER_UNI
 
@@ -212,7 +211,7 @@ def ask_uni(update, context):
     sex = update.message.text
     if sex != 'Чол' and sex != 'Жін':
         return ask_sex(update, context)
-    user.sex = sex
+    context.user_data['sex'] = sex
     keyboard = [[KeyboardButton('Національний Університет "Львівська Політехніка"')],
                 [KeyboardButton('Львівський Національний Університет ім. І.Франка')],
                 [KeyboardButton('Національний Лісотехнічний Університет України')],
@@ -221,76 +220,76 @@ def ask_uni(update, context):
                 [KeyboardButton('Українська Академія Друкарства')],
                 ]
     context.bot.send_message(chat_id=update.message.chat_id,
-                             text=get_question(6),
+                             text=ASK_UNI_TEXT,
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return ENTER_COURSE
 
 
 def ask_course(update, context):
-    user.uni = update.message.text
+    context.user_data['uni'] = update.message.text
     keyboard = [[KeyboardButton('1'), KeyboardButton('2'), KeyboardButton('3')],
                 [KeyboardButton('4'), KeyboardButton('5'), KeyboardButton('6')],
                 [KeyboardButton('Закінчив'), KeyboardButton('Школяр')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(7),
+    context.bot.send_message(chat_id=update.message.chat_id, text=ASK_COURSE_TEXT,
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return ENTER_VISITED
 
 
 def ask_visited(update, context):
-    course = update.message.text
-    user.course = course
-    user.course = update.message.text
+    context.user_data['course'] = update.message.text
     keyboard = [[KeyboardButton('Так'), KeyboardButton('НІ')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(8),
+    context.bot.send_message(chat_id=update.message.chat_id, text=ASK_VISITED_TEXT,
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return SPECIFY_VISITED
 
 
 def specify_visited(update, context):
-    user.visited = update.message.text
-    if not (user.visited == 'Так'):
+    context.user_data['visited'] = update.message.text
+    if not (context.user_data['visited'] == 'Так'):
         return ask_how_come(update, context)
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(9))
+    context.bot.send_message(chat_id=update.message.chat_id, text=SPECIFY_VISITED_TEXT)
     return ENTER_HOW_COME
 
 
 def ask_how_come(update, context):
     if user.visited == 'Так':
-        user.visited = update.message.text
+        context.user_data['visited'] = update.message.text
     keyboard = [[KeyboardButton('Флаєр')], [KeyboardButton('Постер')], [KeyboardButton('Друзі запросили')],
                 [KeyboardButton('Реклама Instagram')], [KeyboardButton('Реклама Telegram')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(10),
+    context.bot.send_message(chat_id=update.message.chat_id, text=ASK_HOW_COME_TEXT,
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return ENTER_ENGLISH_LEVEL
 
 
 def ask_english_level(update, context):
-    user.how_come = update.message.text
+    context.user_data['how_come'] = update.message.text
     keyboard = [[KeyboardButton('повний нуль 🙂')],
                 [KeyboardButton('щось розумію, сказати нічого не зможу')],
                 [KeyboardButton('можу скласти кілька речень на загальні теми')],
                 [KeyboardButton('можу вести діалог, але слово consciousness буду гуглити')],
                 [KeyboardButton('дивлюсь серіали англійською без субтитрів')],
                 [KeyboardButton('англійська майже як рідна')]]
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(11),
+    context.bot.send_message(chat_id=update.message.chat_id, text=ASK_ENGLISH_LEVEL_TEXT,
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return ENTER_RELIGIOUS
 
 
 def ask_religious(update, context):
-    user.english_level = update.message.text
+    context.user_data['english_level'] = update.message.text
     keyboard = [[KeyboardButton('Позитивно')],
                 [KeyboardButton('Негативно')],
                 [KeyboardButton('Нейтрально')], ]
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(12),
+    context.bot.send_message(chat_id=update.message.chat_id, text=ASK_RELIGIOUS_TEXT,
                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
     return EXIT_CONVERSATION
 
 
 def exit_conversation(update, context):
-    user.religious = update.message.text
-    update.message.reply_text(get_question(13))
-    add_to_spreadsheets()
+    context.user_data['religious'] = update.message.text
+    update.message.reply_text(END_REGISTRATION_TEXT)
+    add_to_spreadsheets(list(context.user_data.values()))
+    print(context.user_data.values())
+    show_menu(update, context)
     return ConversationHandler.END
 
 
@@ -312,6 +311,7 @@ def ask_message_text(update, context):
     try:
         for chat in chats:
             context.bot.send_message(chat_id=int(chat), text=update.message.text)
+            time.sleep(1)
         context.bot.send_message(chat_id=update.message.chat_id, text='sent')
     except:
         context.bot.send_message(chat_id=int(read_config('ADMIN_ID')), text=traceback.format_exc())
@@ -328,32 +328,32 @@ def show_menu(update, context):
 
 
 def send_location(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(14))
+    context.bot.send_message(chat_id=update.message.chat_id, text=LOCATION_TEXT, parse_mode=ParseMode.HTML)
 
 
 def send_schedule(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(15))
+    context.bot.send_message(chat_id=update.message.chat_id, text=SCHEDULE_TEXT)
 
 
 def send_interview(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(16))
+    context.bot.send_message(chat_id=update.message.chat_id, text=INTERVIEW_TEXT)
 
 
 def send_tutor_time(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(17))
+    context.bot.send_message(chat_id=update.message.chat_id, text=TUTOR_TIME_TEXT)
 
 
 def send_about_us(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(18))
+    context.bot.send_message(chat_id=update.message.chat_id, text=ABOUT_US_TEXT)
 
 
 def send_connect(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text=get_question(19))
+    context.bot.send_message(chat_id=update.message.chat_id, text=CONNECT_TEXT)
 
 
 def main():
     print("start")
-    updater = Updater(read_config("BOT_TOKEN"), use_context=True)
+    updater = Updater(read_config("TEST_BOT_TOKEN"), use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start_command))
     dispatcher.add_handler(CommandHandler('menu', show_menu))
